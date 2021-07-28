e06442dacfe588faadfb9d6266310ff564399b1c
gzlupko <gianzlupko@gmail.com>
2021-07-28 08:30:46 -0400

    sample sentiment analyses on twitter data

---
+++ sample_sentiment_analyses.R
@@ -0,0 +1,32 @@
+# Sample Sentiment Analysis 
+
+
+library(tidytext) 
+library(tidyverse) 
+
+
+airline_tweets <- readRDS("sample_twitter_data.rds") 
+
+
+# split columns into tokens; one token per row 
+tidy_review <- airline_tweets %>% 
+  unnest_tokens(word, tweet_text) %>%
+  anti_join(stop_words) %>%
+  inner_join(get_sentiments("nrc")) 
+
+# 
+
+tidy_review %>%
+  select(word, sentiment) %>% 
+  group_by(sentiment) %>% 
+  count(sentiment) %>%
+  top_n(n) %>%
+  arrange(desc(n)) 
+
+tidy_review %>%
+  count(sentiment, sort = T) 
+
+
+tidy_review %>%
+  count(word) %>%
+  arrange(desc(n)) 
