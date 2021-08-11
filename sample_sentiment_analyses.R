8e7a1fda1b1f81d4f210c8983f0cd6eaca4dfadf
gzlupko <gianzlupko@gmail.com>
2021-08-10 22:49:05 -0400

    analyzed reasons and counterarguments data and conduct diff. in proportions z-test

--- sample_sentiment_analyses.R
+++ sample_sentiment_analyses.R
@@ -39,3 +39,65 @@ tidy_review %>%
   kbl() %>%
   kable_styling() %>%
   save_kable(file = "tidy_review.png")  
+
+
+
+# load reasons data 
+
+reasons_data <- read.csv("reasons_data_8.10.csv") 
+reasons_combined <- read.csv("reasons_combined.csv") 
+# convert to character 
+
+reasons_combined$Reasons <- as.vector(reasons_combined$Reasons) 
+
+
+
+# input reasons for data into sentiment pipe 
+tidy_reasons <- reasons_combined %>% 
+  unnest_tokens(word, Reasons) %>%
+  anti_join(stop_words) %>%
+  inner_join(get_sentiments("nrc")) %>%
+  select(word, sentiment) %>% 
+  group_by(sentiment) %>% 
+  count(sentiment) %>%
+  top_n(n) %>%
+  arrange(desc(n)) 
+ 
+tidy_reasons
+
+# total number of reasons analyzed
+sum(tidy_reasons$n) 
+
+# load counterargumnents combined responses 
+
+counters_combined <- read.csv("counterarguments_combined.csv") 
+# change response data type to character string 
+counters_combined$Counterarguments <- as.vector(counters_combined$Counterarguments) 
+# input counterarguments into sentiment pipe 
+
+
+tidy_counters <- counters_combined %>% 
+  unnest_tokens(word, Counterarguments) %>%
+  anti_join(stop_words) %>%
+  inner_join(get_sentiments("nrc")) %>%
+  select(word, sentiment) %>% 
+  group_by(sentiment) %>% 
+  count(sentiment) %>%
+  top_n(n) %>%
+  arrange(desc(n)) 
+
+tidy_counters
+# total number of counterarguments analyzed 
+sum(tidy_counters$n) 
+
+# difference of proportion test: positive 
+# first look at sum of positive and overall n size for both 
+
+sum(tidy_reasons$n) 
+tidy_reasons 
+sum(tidy_counters$n)
+tidy_counters
+prop_test_positive <- prop.test(x = c(377,232), n = c(1359,1297))
+prop_test_positive 
+
+
