b001ed8e303001c4180dae1bde2a89aa21b1b4e1
gzlupko <gianzlupko@gmail.com>
2021-09-20 23:33:33 -0400

    added text cleaning steps to consider for future pre-processing

--- Twitter_study/twitter_study_pilot.R
+++ Twitter_study/twitter_study_pilot.R
@@ -1,11 +1,19 @@
 # twitter sample 
 
 
-# create Twitter connection 
 
+##### Libraries 
 
 library(ROAuth)
 library(twitteR)
+library(tidyverse) 
+library(tidytext) 
+library(tidyverse)
+library(topicmodels) 
+library(ldatuning)
+
+# create Twitter connection 
+
 #api_key <- "jcf0K4bRqDrTVLD1sSZmsdWLI"
 #api_secret <- "ZTU2urYwNj8RMXqSYZJXi5Gveb8GbSP0zITT9vqLJ2mOUgh7po"
 access_token <- "363179894-ZuN7k6hrw8SucTZtwKQXxcp3aZ2EUg1onZWcMvwx"
@@ -27,4 +35,82 @@ wfh_search<- searchTwitter(terms_search, n = 10, lang="en")
 wfh_df <- twListToDF(wfh_search)
 View(wfh_df) 
 ?searchTwitteR
+
+
+
+
+# text pre-processing 
+
+
+clean_tweets <- function(x) {
+  x %>%
+    # Remove URLs
+    str_remove_all(" ?(f|ht)(tp)(s?)(://)(.*)[.|/](.*)") %>%
+    # Remove mentions e.g. "@my_account"
+    str_remove_all("@[[:alnum:]_]{4,}") %>%
+    # Remove hashtags
+    str_remove_all("#[[:alnum:]_]+") %>%
+    # Replace "&" character reference with "and"
+    str_replace_all("&amp;", "and") %>%
+    # Remove puntucation, using a standard character class
+    str_remove_all("[[:punct:]]") %>%
+    # Remove "RT: " from beginning of retweets
+    str_remove_all("^RT:? ") %>%
+    # Replace any newline characters with a space
+    str_replace_all("\\\n", " ") %>%
+    # Make everything lowercase
+    str_to_lower() %>%
+    # Remove any trailing whitespace around the text
+    str_trim("both")
+}
+
+clean_tweets(tidy_tweets$word) 
+
+
+# remove unwanted terms 
+
+
+
+remove_reg <- "&amp;|&lt;|&gt;"
+tidy_tweets <- wfh_df %>% 
+  filter(!str_detect(text, "^RT")) %>%
+  mutate(text = str_remove_all(text, remove_reg)) %>%
+  unnest_tokens(word, text) %>%
+  filter(!word %in% stop_words$word,
+         !word %in% str_remove_all(stop_words$word, "'"),
+         str_detect(word, "[a-z]"))
+
+tidy_tweets$word
+
+
+# create Document Term Matrix for LDA 
+
+sample_dtm <- tidy_tweets %>%
+  count(id, word) %>%
+  cast_dtm(document = id, term = word, value = n) %>%
+  as.matrix() 
+
+#sample_dtm <- wfh_df %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words) %>%
+  count(id, word) %>%
+  cast_dtm(document = id, term = word, value = n) %>%
+  as.matrix() 
+
+# run LDA 
+lda_1 <- LDA(
+  sample_dtm, 
+  k = 2, 
+  method = "Gibbs"
+  
+)
+
+
+lda_1_topics <- lda_1 %>%
+  tidy(matrix = "beta") 
+
+# arrange topics descending
+
+lda_1_topics %>%
+  arrange(desc(beta)) 
 
