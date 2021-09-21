cab370bab40450675368f9344b37c54e6992f536
gzlupko <gianzlupko@gmail.com>
2021-09-20 23:11:51 -0400

    test run with successful twitter pull

--- Twitter_study/twitter_study_pilot.R
+++ Twitter_study/twitter_study_pilot.R
@@ -1,22 +1,30 @@
 # twitter sample 
 
-library(ROAuth)
-library(twitteR)
-
 
 # create Twitter connection 
 
-api_key <- "jcf0K4bRqDrTVLD1sSZmsdWLI"
-api_secret <- "ZTU2urYwNj8RMXqSYZJXi5Gveb8GbSP0zITT9vqLJ2mOUgh7po"
-access_token <- "AAAAAAAAAAAAAAAAAAAAAFedTwEAAAAAeiHNm98VAp1WXC6H1l3kt093Jkw%3DRFFhFgrUm24KpY04BYK6xHUoazVq0uYuPIOEzvPUA9a2RHpYJB"
-access_token_secret <- "38jeqJKRAFCwUY8BdJTMGe2OHWEldJ7q55la7hINoNePA"
-bearer_token <- "AAAAAAAAAAAAAAAAAAAAAFedTwEAAAAAeiHNm98VAp1WXC6H1l3kt093Jkw%3DRFFhFgrUm24KpY04BYK6xHUoazVq0uYuPIOEzvPUA9a2RHpYJB"
-setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 
+library(ROAuth)
+library(twitteR)
+#api_key <- "jcf0K4bRqDrTVLD1sSZmsdWLI"
+#api_secret <- "ZTU2urYwNj8RMXqSYZJXi5Gveb8GbSP0zITT9vqLJ2mOUgh7po"
+access_token <- "363179894-ZuN7k6hrw8SucTZtwKQXxcp3aZ2EUg1onZWcMvwx"
+access_secret <- "WRHp26YYyOyq01GrA9kSJvj8tx9enGWg9UiiPNehf4BIW"
+consumer_key <- "eq3R1STvoYHXkqGuHoeQpAzmU"
+consumer_secret <- "R2RYGZbAcDFbA1a34kssLk3mnRClrvjvQcd2nc0d0cRco4l2bj" 
 
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret) 
 
-### Download sample tweets
-```{r}
+###### Store searchString
 
-TL <- searchTwitter("work from home", n=2, since='2021-07-01', until='2021-08-01')
-TL <- do.call("rbind", lapply(TL, as.data.frame))
+terms <- c("telework", "teleworking", "teleworked", "remote work", "remote working", 
+           "remotework", "remoteworking", "work remote", "remoteworking", "work remote", 
+           "worked remote", "work remotely", "working remotely", "worked remotely", 
+           "workremote", "workingremote", "workedremote", "WFH", "workfromhome", "work from home", 
+           "teleworker", "worked from home", "workedfromhome") 
+terms_search <- paste(terms, collapse = " OR ")
+wfh_search<- searchTwitter(terms_search, n = 10, lang="en")
+wfh_df <- twListToDF(wfh_search)
+View(wfh_df) 
+?searchTwitteR
+
